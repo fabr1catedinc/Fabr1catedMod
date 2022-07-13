fe14fa0303780b8aaecbf581a1bb25137d3112be
BennovanDorst <bennovandorst@gmail.com>
2022-07-13 18:56:56 +0200

    [❓] Added uh well.. kawaii...

--- src/main/java/me/fabr1cated/fabricatedmod/Init/ItemInit.java
+++ src/main/java/me/fabr1cated/fabricatedmod/Init/ItemInit.java
@@ -1,6 +1,7 @@
 package me.fabr1cated.fabricatedmod.Init;
 
 import me.fabr1cated.fabricatedmod.FabricatedMod;
+import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,5 +12,6 @@ public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FabricatedMod.MODID);
 
     public static final RegistryObject<Item> FABRICATED_KEYS = ITEMS.register("fabricated_keys", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+    public static final RegistryObject<Item> KAWAII = ITEMS.register("kawaii", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(Foods.ENCHANTED_GOLDEN_APPLE)));
 }
 
