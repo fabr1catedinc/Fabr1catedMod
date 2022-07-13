a8d4465483e2c2221ef57cd4d7d2216bbd68274e
BennovanDorst <bennovandorst@gmail.com>
2022-07-13 17:26:16 +0200

    [🎉] First Commit

---
+++ src/main/java/me/fabr1cated/fabricatedmod/FabricatedMod.java
@@ -0,0 +1,8 @@
+package me.fabr1cated.fabricatedmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FabricatedMod.MODID)
+public class FabricatedMod {
+    public static final String MODID = "fabricatedmod";
+}
